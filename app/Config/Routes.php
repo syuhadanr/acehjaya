875f2b1551070f0d43b0c4d640b05b735e815b6d
unknown <monhadzan@gmail.com>
2024-05-27 15:10:35 +0700

    integrasi template

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -6,3 +6,7 @@ use CodeIgniter\Router\RouteCollection;
  * @var RouteCollection $routes
  */
 $routes->get('/', 'Home::index');
+$routes->get('/about', 'Home::about');
+$routes->get('/service', 'Home::service');
+$routes->get('/contact', 'Home::contact');
+$routes->get('/project', 'Home::project');
--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -6,6 +6,26 @@ class Home extends BaseController
 {
     public function index(): string
     {
-        return view('welcome_message');
+        return view('index');
+    }
+
+    public function about(): string
+    {
+        return view('about');
+    }
+
+    public function service(): string
+    {
+        return view('service');
+    }
+
+    public function project(): string
+    {
+        return view('project');
+    }
+
+    public function contact(): string
+    {
+        return view('contact');
     }
 }
